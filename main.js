2d31d5b0a4dfd0daf23a7ed50a8d64a47df6095f
mee96 <carmenmc96@gmail.com>
2024-11-07 12:13:31 +0100

    un anime

--- main.js
+++ main.js
@@ -2,17 +2,37 @@
 
 console.log("konichiwa sekai");
 
-
-
-
 const list = document.querySelector(".js-list");
+const form = document.querySelector(".anime-form");
+const nameanime = document.querySelector(".anime-name");
 
 
-fetch (" https://api.jikan.moe/v4/anime?q=naruto")
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const nombre = nameanime.value;
+    console.log(nombre);
+
+
+
+fetch (`https://api.jikan.moe/v4/anime?q=${nombre}`)
     .then (Response => Response.json())
     .then (data => {
         console.log(data); //confirmo que pido bien los datos al servidor
         const animes = data.data; //accedo a los datos que necesito
         const anime = animes[0];
         console.log(anime);
+        list.innerHTML = `
+        <div>
+
+            <h5>${anime.title} ${anime.title_japanese}</h5>
+            <img src="${anime.images.jpg.image_url}" alt="Portada serie">
+
+        </div> 
+         `
+// pintamos en el html con los datos del api 
+
+//for? para los titulos con varias series como naturo, soa, bleach, etc ?
+        
     });
+
+    })
