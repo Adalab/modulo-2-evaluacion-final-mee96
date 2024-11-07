6b3b5744a32458de872f3fec621b5bdaca84eec2
mee96 <carmenmc96@gmail.com>
2024-11-08 00:12:24 +0100

    favs

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-'use strict';
 
 console.log("konichiwa sekai");
 
 const list = document.querySelector(".js-list");
+const listfav = document.querySelector(".js-list-fav");
 const form = document.querySelector(".anime-form");
 const nameanime = document.querySelector(".anime-name");
 let animeList = [];
@@ -15,6 +15,35 @@ form.addEventListener("submit", (event) => {
     console.log(nombre);
 
 
+    function renderAnime(animes) {
+        for (const anime of animes) {
+            let imageUrl = anime.images.jpg.image_url;  //es un let perqe lo cambiem
+
+            if (imageUrl === "https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png") {
+
+                imageUrl = "https://media.istockphoto.com/id/1349233065/vector/404-error-page-not-found-sad-kawaii-bunny-and-duckling-with-err.jpg?s=612x612&w=0&k=20&c=HxgSK3akep5Jci8AL-6Ku4-T6LRvR7IZK9oYQKnvvR0=";
+            } //si la url es la primera la cambio per la segona
+
+
+            const listanime = `
+                    <div class="js-anime" id=${anime.mal_id}>
+                        <h5>${anime.title} ${anime.title_japanese}</h5>
+                        <img src="${imageUrl}" alt="Portada serie" width="210" height="295">
+                    </div> 
+                `;
+
+            list.innerHTML += listanime;
+
+            const allanimesDOM = document.querySelectorAll(".js-anime");
+            //posem el query selector ALL perqe cada div sera un anime i aixi escoltem tots, dins dels fetch perqe es asincrono i esta en segon pla
+            for (const animeDOM of allanimesDOM) {
+                animeDOM.addEventListener("click", handleAddFavorite);
+            }
+
+        }
+
+    }
+
     fetch(`https://api.jikan.moe/v4/anime?q=${nombre}`)
         .then(response => response.json())
         .then(data => {
@@ -24,37 +53,13 @@ form.addEventListener("submit", (event) => {
             list.innerHTML = ""; // borro lo anterior per a que no se me sumen cada vegada
 
 
-            for (const anime of animes) {
+            renderAnime(animes);
 
-                let imageUrl = anime.images.jpg.image_url;  //es un let perqe lo cambiem
-
-                if (imageUrl === "https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png") {
-
-                    imageUrl = "https://media.istockphoto.com/id/1349233065/vector/404-error-page-not-found-sad-kawaii-bunny-and-duckling-with-err.jpg?s=612x612&w=0&k=20&c=HxgSK3akep5Jci8AL-6Ku4-T6LRvR7IZK9oYQKnvvR0=";
-                } //si la url es la primera la cambio per la segona
-
-
-                const listanime = `
-                    <div class="js-anime" id=${anime.mal_id}>
-                        <h5>${anime.title} ${anime.title_japanese}</h5>
-                        <img src="${imageUrl}" alt="Portada serie" width="210" height="295">
-                    </div> 
-                `;
-
-
-                list.innerHTML += listanime;
-
-                const allanimesDOM = document.querySelectorAll(".js-anime");
-                //posem el query selector ALL perqe cada div sera un anime i aixi escoltem tots, dins dels fetch perqe es asincrono i esta en segon pla
-                for (const animeDOM of allanimesDOM) {
-                    animeDOM.addEventListener("click", handleAddFavorite);
-                }
-
-            }
-        })
+        }
+        )
     function handleAddFavorite(event) {
         console.log("click en un anime");
-        console.log(event.currentTarget.id);
+        console.log(event.currentTarget.id);//current target perqe aixi agafo larray sancer y no nomes la part on he clickat
         const idAnimeClicked = event.currentTarget.id;
         //const AnimeSelected = 
     }
