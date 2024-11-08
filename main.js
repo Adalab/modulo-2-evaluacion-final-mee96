d1f1385d9eb86d667a68fb1a144c07171e831787
mee96 <carmenmc96@gmail.com>
2024-11-08 03:58:42 +0100

    id

--- main.js
+++ main.js
@@ -18,6 +18,7 @@ form.addEventListener("submit", (event) => {
 
     function renderAnime(animes) {
         for (const anime of animes) {
+
             let imageUrl = anime.images.jpg.image_url;  //es un let perqe lo cambiem
 
             if (imageUrl === "https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png") {
@@ -35,6 +36,12 @@ form.addEventListener("submit", (event) => {
 
             list.innerHTML += listanime;
 
+            //click per cada paleta
+            const allanimesDOM = document.querySelectorAll(".js-anime");
+            for (const animeDOM of allanimesDOM) {
+                animeDOM.addEventListener("click", handleAddFavorite);
+            }
+
         }
 
     }
@@ -44,47 +51,24 @@ form.addEventListener("submit", (event) => {
         .then(data => {
             console.log(data); // Confirmo que demano be les dades al servidor
             const animes = data.data; // Accedeixo a les dades que necesito
-            localStorage.setItem("animeInfo", JSON.stringify(animes)); //guardo al LS
-            list.innerHTML = ""; // borro lo anterior per a que no se me sumen cada vegada
 
-            const allanimesDOM = document.querySelectorAll(".js-anime");
-            //posem el query selector ALL perqe cada div sera un anime i aixi escoltem tots, dins dels fetch perqe es asincrono i esta en segon pla
-            for (const animeDOM of allanimesDOM) {
-                animeDOM.addEventListener("click", handleAddFavorite);
-            }
             renderAnime(animes);
 
         }
         )
+
+    //animes preferits: 
     function handleAddFavorite(event) {
         console.log("click en un anime");
         console.log(event.currentTarget.id);
         const idAnimeClicked = event.currentTarget.id;
-        const AnimeSelected = animeList.find((anime) => {
+        const animeSelected = animeList.find((anime) => {
             return anime.id === idAnimeClicked;
         })
-        favoritesAnimesList.push(AnimeSelected);
+        favoritesAnimesList.push(animeSelected);
+        console.log(animeSelected); // me torna undefinded i no array
 
-        listfav.innerHTML = "";
-        for (const anime of favoritesAnimesList) {
-            /*let imageUrl = anime.images.jpg.image_url;  //es un let perqe lo cambiem
 
-            if (imageUrl === "https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png") {
-
-                imageUrl = "https://media.istockphoto.com/id/1349233065/vector/404-error-page-not-found-sad-kawaii-bunny-and-duckling-with-err.jpg?s=612x612&w=0&k=20&c=HxgSK3akep5Jci8AL-6Ku4-T6LRvR7IZK9oYQKnvvR0=";
-            } //si la url es la primera la cambio per la segona
-
-                    */
-            const listanimefav = `
-                    <div class="js-anime" id=${anime.mal_id}>
-                        <h5>${anime.title} ${anime.title_japanese}</h5>
-                        <img src="${imageUrl}" alt="Portada serie" width="210" height="295">
-                    </div> 
-                `;
-
-            listfav.innerHTML += listanimefav;
-
-        }
 
     }
 
@@ -95,10 +79,10 @@ form.addEventListener("submit", (event) => {
 
 
 //Local storage, si la usuaria ja hagues entrat anteriorment no caldria carregar el fetch i ahorrem temps
-const animesLocalStorage = localStorage.getItem("animeInfo");
+/*const animesLocalStorage = localStorage.getItem("animeInfo");
 console.log(animesLocalStorage);
 
 if (animesLocalStorage !== null) {
     //renderAnime(animes);
 
-}
+}*/
